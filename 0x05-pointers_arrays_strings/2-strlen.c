68b5c1560986a9421c3e62e1175a7c68dbed01b4
Bit <Bit@mail.com>
2023-09-19 17:42:45 -0700

    committing file 2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+ * strlen - a function to return the length of var
+ * @str: pointer that holds string value
+ *
+ * Return: len
+ */
+int _strlen(char *str)
+{
+	strlen(str);	
+return (*str);
+}
