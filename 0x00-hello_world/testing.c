e33a20c733d4db55d6c1eb5501f8441740710362
BiTvm <BiTvm@github.com>
2024-02-20 22:45:34 +0200

    adding 0x00-hello_world

---
+++ 0x00-hello_world/testing.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - testing file!
+ * description: used to Ensure that everthing yr doing
+ * at this moment(piece by piece, bit by bit) is working fine till this point!
+ *
+ * Return: Always (0)!
+ */
+int main(void)
+{
+	printf("it's what it's");
+return (0);
+}
