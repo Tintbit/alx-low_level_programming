2b9a9e80078b5c2fed63592b7f641681c49d76ec
BiTvm <BiTvm@github.com>
2024-02-22 22:08:04 +0200

    5/6 printf & size done!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - returns int value, takes no arg
+ * description: using printf built-in function!
+ *
+ * Retrun: Always (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
