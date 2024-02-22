fd7702994a4db9060166673302b21ed9001a922b
BiTvm <BiTvm@github.com>
2024-02-22 21:19:35 +0200

    4-puts.c is working!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - returns int value, takes no arg!
+ * description: output msg to stdout stream
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
