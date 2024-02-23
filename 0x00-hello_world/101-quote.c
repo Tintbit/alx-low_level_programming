bb4cbee817c42073463217179fe9db71b0f589f4
BiTvm <BiTvm@github.com>
2024-02-23 15:01:12 +0200

    Alternate return(1) than return(0)? why?

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,15 @@
 /**
  * main - printing some quotes to std
  * description: using fwrite or write, or even system() from stdlib.h
- * @quote: array of char
+ * void : func parameter!
  *
  * Return: Always (0)
  */
 int main(void)
 {
-	static const char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(quote, sizeof(quote)-1, 1, stderr);
-	return (0);
+static const char
+	q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+fwrite(q, sizeof(q) - 1, 1, stderr);
+return (1);
 }
