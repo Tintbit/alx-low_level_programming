bd854ad9145a8879b3fffdcb1ff4ed207da01e87
BitContainer <BitContainer@gmail.com>
2024-07-13 15:52:29 +0000

    PJ Task: 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -32,7 +32,7 @@ int main(void)
 	{
 		printf("%d and is greater than 5 \n", last_digit);
 	}
-	else if (last_digit < 6)
+	else if (last_digit < 6 && last_digit != 0)
 	{
 		printf("%d and is less than 6 and not 0\n", last_digit);
 	}
